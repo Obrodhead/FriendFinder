c58c2c240f2072fbf7cac5d7a53e73f8fd8b2568
Obrodhead <50184145+Obrodhead@users.noreply.github.com>
2019-08-08 17:22:02 -0400

    commit w error remaining

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+    {
+        "name": "Elmo",
+        "photo": "https://cdn.shopify.com/s/files/1/0894/6582/products/bh6868-1000_600x.jpg?v=1559617669",
+        "scores": [
+            1,
+            5,
+            3,
+            3,
+            4,
+            5,
+            1,
+            4,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "Spongebob",
+        "photo": "https://www.cartoonbrew.com/wp-content/uploads/2018/10/spongebob_movie_wonderfulsponge-1280x600.jpg",
+        "scores": [
+            2,
+            3,
+            1,
+            1,
+            5,
+            1,
+            5,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Timmy Turner",
+        "photo": "https://giantbomb1.cbsistatic.com/uploads/scale_small/0/5768/701408-timmy_turner.jpg",
+        "scores": [
+            4,
+            5,
+            3,
+            2,
+            1,
+            5,
+            5,
+            1,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Bart Simpson",
+        "photo": "https://media.proprofs.com/images/QM/user_images/2169923/1514451045.png",
+        "scores": [
+            2,
+            3,
+            5,
+            4,
+            5,
+            1,
+            4,
+            2,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Angelica Pickles",
+        "photo": "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit/cdn/4b8e4280-f541-48ab-84d1-433d6a763256/1ef14ed0-1fcd-47ee-85cc-b690d1ba7b7b_560_420.jpg",
+        "scores": [
+            1,
+            5,
+            5,
+            5,
+            1,
+            1,
+            2,
+            5,
+            4,
+            5
+        ]
+    }
+
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+var friendList = require("../data/friends.js");
+path = require("path");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendList);
+      });
+    
+      app.post("/api/friends", function(req, res) {
+        var scoresArray = [];
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: scoresArray
+        };
+        for (i = 0; i < req.body.scores.length; i++) {
+            scoresArray.push(parseInt(req.body.scores[i]))
+        }
+        friendList.push(newFriend);
+      })
+
+};
