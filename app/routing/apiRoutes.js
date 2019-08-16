246b2c73ca9ef133398601a5d619017669fd697e
Obrodhead <50184145+Obrodhead@users.noreply.github.com>
2019-08-16 19:09:15 -0400

    commit w/ changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,10 +14,45 @@ module.exports = function(app) {
             photo: req.body.photo,
             scores: scoresArray
         };
-        for (i = 0; i < req.body.scores.length; i++) {
-            scoresArray.push(parseInt(req.body.scores[i]))
-        }
-        friendList.push(newFriend);
+        var bestMatch = {
+          name: "",
+          photo: "",
+          friendDifference: Infinity
+        };
+     
+     
+        var currentBest = 100;
+        var differenceArray = [];
+        for (i = 0; i < friendList.length -1; i++ ) {
+          
+            for(j = 0; j < 10; j++) {
+                var friendsDifference = newFriend.scores[j] - friendList[i].scores[j];
+                differenceArray.push(Math.abs(friendsDifference));
+               
+            }
+            var difference = differenceArray.reduce(function(total,amount){
+                return total + amount
+            });
+            console.log(difference);
+
+            difference=50;
+            
+            if (difference < currentBest) {
+                bestFriend = friendList[i];
+                currentBest = difference;
+                differenceArray = [];
+            }
+            else {
+            differenceArray = [];
+            }
+          }
+          console.log(bestMatch);
+          res.json(bestMatch);
+          
+        // for (i = 0; i < req.body.scores.length; i++) {
+        //     scoresArray.push(parseInt(req.body.scores[i]))
+        // // }
+        // friendList.push(newFriend);
       })
 
 };
